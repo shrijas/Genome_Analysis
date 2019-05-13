30f27e79a3ecddc278d214fc10ef30a2ba7961a0
shrijas <suju6474@gmail.com>
2019-05-13 20:32:26 +0200

    FastQC result

--- scripts/rnaseq_fastqc.sh
+++ scripts/rnaseq_fastqc.sh
@@ -3,15 +3,13 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 0:05:00
-#SBATCH -J run_fastqc
+#SBATCH -J fastqc_runcheck
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Shrija.Srinivasan.0481@student.uu.se
-
 #Load Molecules
 module load bioinfo-tools
 module load FastQC/0.11.5
-
-#Your commands
+#Commands
 for filename in /home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/raw/*.fastq.gz
 	do
 		fastqc $filename -o /home/shsr0481/Genome_Analysis/results/1_Qualitycontrol_RNA/rnaseq_fastqc;

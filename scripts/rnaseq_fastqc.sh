33157c04fde11c93ecbc5851ec81ab6e8258fde0
shrijas <suju6474@gmail.com>
2019-05-13 20:55:38 +0200

    FastQC result

--- scripts/rnaseq_fastqc.sh
+++ scripts/rnaseq_fastqc.sh
@@ -1,7 +1,9 @@
 #!/bin/bash -L
+#SBATCH -A 2019003
+#SBATCH -J runchecker_FASTQC
 #Commands
 for filename in /home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/raw/*.fastq.gz
 do
 	echo "Running $filename ..."
-	fastqc $filename -t 1 -o /home/shsr0481/Genome_Analysis/results/1_Qualitycontrol_RNA/rnaseq_fastqc "$filename"
+	fastqc -t 1 -o /home/shsr0481/Genome_Analysis/results/1_Qualitycontrol_RNA/rnaseq_fastqc "$filename"
 done

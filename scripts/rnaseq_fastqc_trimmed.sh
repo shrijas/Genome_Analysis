7ced9d83402fcdc687ab52cce2f463b736ea2fb0
shrijas <suju6474@gmail.com>
2019-05-14 20:46:36 +0200

    FastQC result_Trimmed

--- scripts/rnaseq_fastqc_trimmed.sh
+++ scripts/rnaseq_fastqc_trimmed.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 2
-#SBATCH -t 00:15:00
+#SBATCH -t 01:30:00
 #SBATCH -J fastqc_checker
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Shrija.Srinivasan.0481@student.uu.se

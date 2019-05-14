65f5b455a0b530038582119e38dc81f8299f08fd
shrijas <suju6474@gmail.com>
2019-05-14 21:07:17 +0200

    FastQC result_Trimmed

--- scripts/rnaseq_fastqc_trimmed.sh
+++ scripts/rnaseq_fastqc_trimmed.sh
@@ -3,7 +3,7 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 01:30:00
-#SBATCH -J fastqc_checker
+#SBATCH -J fastqc_trimmedcheck
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Shrija.Srinivasan.0481@student.uu.se
 #Modules
@@ -14,5 +14,5 @@ module load FastQC/0.11.5
 for filename in /home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/*.fastq.gz
 do
 	echo "Running $filename ..."
-	fastqc -o /home/shsr0481/Genome_Analysis/results/1_Qualitycontrol_RNA/rnaseq_fastqc/trimmed "$filename"
+	fastqc -t 1 -o /home/shsr0481/Genome_Analysis/results/1_Qualitycontrol_RNA/rnaseq_fastqc/trimmed "$filename"
 done

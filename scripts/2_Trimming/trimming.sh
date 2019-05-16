62bf8dda3402378e2cce00d5ef01c32c4d72b6e2
shrijas <suju6474@gmail.com>
2019-05-16 13:58:57 +0200

    Trimmomatic procssing

--- scripts/2_Trimming/trimming.sh
+++ scripts/2_Trimming/trimming.sh
@@ -3,26 +3,21 @@
 #SBATCH -p core
 #SBATCH -n 2
 #SBATCH -t 01:00:00
-#SBATCH -J fastqc_chip_seq_data
+#SBATCH -J Trimmomatic_process
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Shrija.Srinivasan.0481@student.uu.se
-#Load Modules
+#Modules
 module load bioinfo-tools
-module load trimmomatic
+module load FastQC/0.11.5
 
-#Comments
-declare -i count
-count=0
-	for value1 in /home/shsr0481/Genome_Analysis/raw_data/sel3/wgs_data/*.fastq.gz
-		do
-			echo "$count"
+#Your commands
+java -jar /sw/apps/bioinfo/trimmomatic/0.36/rackham/trimmomatic-0.36.jar PE -phred33 \
+/home/shsr0481/Genome_Analysis/data/rna_seq_data/raw/sel3/sel3_SRR1719266.1.fastq.gz \
+/home/shsr0481/Genome_Analysis/data/rna_seq_data/raw/sel3/sel3_SRR1719266.2.fastq.gz \
+/home/shsr0481/Genome_Analysis/results/2_Trimming/rna_seq_data/raw/sel3_SRR1719266.trim_1P.fastq.gz \
+/home/shsr0481/Genome_Analysis/results/2_Trimming/rna_seq_data/raw/sel3_SRR1719266.trim_1U.fastq.gz \
+/home/shsr0481/Genome_Analysis/results/2_Trimming/rna_seq_data/raw/sel3_SRR1719266.trim_2P.fastq.gz \
+/home/shsr0481/Genome_Analysis/results/2_Trimming/rna_seq_data/raw/sel3_SRR1719266.trim_2U.fastq.gz \
 
-		if [ $((count % 2)) == 0 ]; then
-			#statements
-			echo "Running $value1 ..."
-			echo "$((value1+1))"
-			value2=$((value1+1))
-			echo "Running ${!value2}"
-		fi
-		let count++
-	done
+#Above 4 lines are output files
+ILLUMINACLIP:TruSeq3-PE.fa:2:30:10 LEADING:3 TRAILING:3 SLIDINGWINDOW:4:15 MINLEN:36

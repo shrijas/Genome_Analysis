280afd54bbe23c5cc8901b94718c3b670a151460
shrijas <suju6474@gmail.com>
2019-05-17 12:59:06 +0200

    SPAdes assembly added

--- scripts/3_Genome_Assembly/SOAPdenovo/soapdenovo_gapcloser.sh
+++ scripts/3_Genome_Assembly/SOAPdenovo/soapdenovo_gapcloser.sh
@@ -1,7 +1,7 @@
 #!bin/batch
 #SBATCH -A g2019003
 #SBATCH -p core
-#SBATCH -n 2
+#SBATCH -n 4
 #SBATCH -t 03:00:00
 #SBATCH -J gapcloser_SOAPdenovo
 #SBATCH --mail-type=ALL
@@ -12,6 +12,8 @@ module load soapdenovo/2.04-r240
 
 #Your comments
 /sw/apps/bioinfo/SOAPdenovo/2.04-r240/rackham/bin/GapCloser \
--b /home/shsr0481/GenomeAnalysis/scripts/3_Genome_Assembly/SOAPdenovo/soapdenovo_config.txt \
+-b /SOAPdenovo/soapdenovo_config.txt \
 -a /home/shsr0481/Genome_Analysis/results/3_Genome_Assembly/SOAPdenovo/*.scafSeq \
--o /home/shsr0481/Genome_Analysis/results/3_Genome_Assembly/SOAPdenovo/Closedgaps_gaps.fasta
+
+
+SOAPdenovo-127mer all -s /home/shsr0481/GenomeAnalysis/scripts/3_Genome_Assembly/soapdenovo_config.txt -K 49 -F -R -o /home/shsr0481/Genome_Analysis/results/3_Genome_Assembly/SOAPdenovo/Closedgaps_gaps.fasta

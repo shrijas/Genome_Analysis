1c722dfb62bd1211c2d9e2abcce54d673f77e92e
shrijas <suju6474@gmail.com>
2019-05-20 21:19:28 +0200

    Trinity unpaired added

---
+++ scripts/4_Transcriptome_Assembly/trinity_unpaired.sh
@@ -0,0 +1,54 @@
+#!/bin/bash -l
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 4:00:00
+#SBATCH -J trinity_unpaired
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Shrija.Srinivasan.0481@student.uu.se
+
+# Load modules
+module load bioinfo-tools
+module load Trinity
+module load samtools/1.9
+module load jellyfish/2.2.6
+module load Salmon/0.9.1
+module load bowtie2/2.3.4.3
+
+/sw/bioinfo/trinity/2.8.2/rackham/Trinity --seqType fq --max_memory 10G \
+--single \
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719013.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719014.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719015.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719016.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719017.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719018.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719204.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719206.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719207.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719208.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719209.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719211.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719212.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719214.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_1U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719242.trim_1U.fastq.gz \
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719013.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719014.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719015.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719016.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719017.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719018.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719204.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719206.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719207.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719208.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719209.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719211.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719212.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719214.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_2U.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719242.trim_2U.fastq.gz \
+--output /home/shsr0481/Genome_Analysis/results/4_transcriptome_assembly/trinity-unpaired

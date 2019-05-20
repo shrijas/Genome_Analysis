909fc9b5992e504cb6af883c9eb7485c252e82cf
shrijas <suju6474@gmail.com>
2019-05-20 13:10:53 +0200

    SOAPdenovo gapcloser added

--- scripts/3_Genome_Assembly/SOAPdenovo/soapdenovo_gapcloser.sh
+++ scripts/3_Genome_Assembly/SOAPdenovo/soapdenovo_gapcloser.sh
@@ -2,7 +2,7 @@
 #SBATCH -A g2019003
 #SBATCH -p core
 #SBATCH -n 4
-#SBATCH -t 03:00:00
+#SBATCH -t 200:00:00
 #SBATCH -J gapcloser_SOAPdenovo
 #SBATCH --mail-type=ALL
 #SBATCH --mail-user Shrija.Srinivasan.0481@student.uu.se
@@ -10,5 +10,6 @@
 module load bioinfo-tools
 module load soapdenovo/2.04-r240
 
-#Your comments
-SOAPdenovo-127mer all -s /home/shsr0481/GenomeAnalysis/scripts/3_Genome_Assembly/soapdenovo_config.txt -K 49 -F -R -o /home/shsr0481/Genome_Analysis/results/3_Genome_Assembly/SOAPdenovo/Closedgaps_gaps.fasta
+# command
+
+GapCloser -b /home/shsr0481/Genome_Analysis/scripts/3_Genome_Assembly/SOAPdenovo/soapdenovo.config -a /home/shsr0481/Genome_Analysis/results/3_Genome_Assembly/SOAPdenovo/.scafSeq -o /home/shsr0481/Genome_Analysis/results/3_Genome_Assembly/SOAPdenovo/GapCloser_output.fasta

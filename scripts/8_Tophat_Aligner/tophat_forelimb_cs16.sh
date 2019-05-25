834d93290844356996fc51be8fdd35c9ecc3b04c
shrijas <suju6474@gmail.com>
2019-05-25 16:38:04 +0200

    Forelimb cs16 added

---
+++ scripts/8_Tophat_Aligner/tophat_forelimb_cs16.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+#SBATCH -A g2019003
+#SBATCH -p core
+#SBATCH -n 2
+#SBATCH -t 10:00:00
+#SBATCH -J forelimb_cs16
+#SBATCH --mail-type=ALL
+#SBATCH --mail-user Shrija.Srinivasan.0481@student.uu.se
+
+### Load modules
+module load bioinfo-tools
+module load tophat
+
+### Your commands
+tophat -o /home/shsr0481/Genome_Analysis/results/8_Tophat_Aligner/forelimb/cs16 \
+/home/shsr0481/Genome_Analysis/results/7_Bowtie/ref \
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719204.trim_1P.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719206.trim_1P.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719207.trim_1P.fastq.gz \
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719204.trim_2P.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719206.trim_2P.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719207.trim_2P.fastq.gz

f9c2f07bfed5c7f8f4f223a14b46bb341977a2e9
shrijas <suju6474@gmail.com>
2019-05-26 20:57:33 +0200

    differential expression- bam sort added

--- scripts/8_Tophat_Aligner/tophat_hindlimb_cs17.sh
+++ scripts/8_Tophat_Aligner/tophat_hindlimb_cs17.sh
@@ -13,10 +13,10 @@ module load tophat
 
 ### Your commands
 tophat -o /home/shsr0481/Genome_Analysis/results/8_Tophat_Aligner/hindlimb/cs17 \
-/home/shsr0481/Genome_Analysis/results/7_Bowtie/ \
-/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_1P.fastq.gz,\
-/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_1P.fastq.gz,\
+/home/shsr0481/Genome_Analysis/results/7_Bowtie/ref \
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_1P.fastq.gz \
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_1P.fastq.gz \
 /home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719206.trim_1P.fastq.gz \
-/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_2P.fastq.gz,\
-/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_2P.fastq.gz,\
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719213.trim_2P.fastq.gz \
+/home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719241.trim_2P.fastq.gz \
 /home/shsr0481/Genome_Analysis/data/raw_data/sel3/rna_seq_data/trimmed/sel3_SRR1719206.trim_2P.fastq.gz
